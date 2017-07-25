84951c0b282bc7ac86812f182bb1eaeabd9cc099
Andriy Havrylyuk <andriy.havrylyuk@gmail.com>
2017-07-25 23:21:38 +0300

    reverse interger: refactored code

--- leatcode/reverse-integer.js
+++ leatcode/reverse-integer.js
@@ -1,16 +1,11 @@
 const reverse = (num) => {
-  const split = (num, base) => {
-    if (num === 0) return []
-    const rem = num % base
-    return [rem].concat(split(num - rem, base * 10))
+  const split = (x) => {
+    if (x === 0) return []
+    const rem = x % 10
+    return [rem].concat(split((x - rem) / 10))
   }
-  const nums = split(num, 10)
-  const reversed = nums.reduce((rev, n, i) => {
-    const div = Math.pow(10, i)
-    const mult = Math.pow(10, nums.length - 1 - i)
-    return rev + n / div * mult
-  }, 0)
+  const reversed = split(num).reduce((rev, x) => 10 * rev + x, 0)
   return Math.abs(reversed) > Math.pow(2, 32 - 1) ? 0 : reversed
 }
 
-a = reverse(-153423646)
+a = reverse(-123)
