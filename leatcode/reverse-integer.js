85c82b8dcf8801e8d4de674dcf710bb57529979a
Andriy Havrylyuk <andriy.havrylyuk@gmail.com>
2017-07-25 23:29:13 +0300

    reverse interger: optimal imperetive solution

--- leatcode/reverse-integer.js
+++ leatcode/reverse-integer.js
@@ -1,11 +1,13 @@
 const reverse = (num) => {
-  const split = (x) => {
-    if (x === 0) return []
-    const rem = x % 10
-    return [rem].concat(split((x - rem) / 10))
+  let rev = 0;
+  const max = Math.pow(2, 32 - 1)
+  while (num !== 0) {
+    let rem = num % 10
+    rev = rev * 10 + rem
+    if (Math.abs(rev) > max) return 0
+    num = (num - rem) / 10
   }
-  const reversed = split(num).reduce((rev, x) => 10 * rev + x, 0)
-  return Math.abs(reversed) > Math.pow(2, 32 - 1) ? 0 : reversed
+  return rev
 }
 
 a = reverse(-123)
