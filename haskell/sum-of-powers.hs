f1cf457840e8ca42f4798914fb1aa10dfeab75d4
Andriy Havrylyuk <andriy.havrylyuk@gmail.com>
2017-07-29 10:40:58 +0300

    sum of powers solved

--- haskell/sum-of-powers.hs
+++ haskell/sum-of-powers.hs
@@ -1,21 +1,14 @@
-selections _ [] = [[]]
-selections target (x:xs) =
-  sel ++ map (x:) sel
-  where sel = selections target xs
-
-sumsTo = sumsTo' 0
+sumOfPowers = sumOfPowers' 1
   where
-    sumsTo' _ _ [] = False
-    sumsTo' s target (x:xs)
-      | null xs && s' == target = True
-      | s' > target = False
-      | otherwise = sumsTo' s' target xs
-      where s' = s + x
-
-powersOf n x = takeWhile (<=x) $ map (^n) [1..]
-sumOfPowers x n = filter (sumsTo x) $ selections x $ powersOf n x
+    sumOfPowers' curr x n
+      | val == 0 = 1
+      | val < 0 = 0
+      | otherwise = sumOfPowers' next val n + sumOfPowers' next x n
+      where
+        val = x - curr ^ n
+        next = curr + 1
 
 main = do
  x <- readLn :: IO Int
  n <- readLn :: IO Int
- print $ length $ sumOfPowers x n
+ print $ sumOfPowers x n
